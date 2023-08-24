529c252e5e795ce8e0857905061713eb38b484a5
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-08-24 18:54:26 +0545

    callout

---
+++ callout.js
@@ -0,0 +1,8 @@
+let btn=document.getElementById("1")
+btn.addEventListener("click",removeCall)
+
+function removeCall()
+{
+    let call=document.getElementById("2")
+    call.style.display="none";
+}
